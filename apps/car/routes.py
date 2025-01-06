81ad68a9a79c7e993b49063d349ae8deb8aa743c
Mel-Rocha <rochamel73@gmail.com>
2025-01-06 20:17:46 -0300

    Centralizando em apps a lógica da aplicação.
    Centralizing application logic in apps.
    
    Централизация логики приложения в приложениях.

--- apps/car/routes.py
+++ apps/car/routes.py
@@ -1,3 +1,4 @@
+import joblib
 import numpy as np
 import pandas as pd
 from fastapi.responses import FileResponse
@@ -9,9 +10,10 @@ from apps.utils.funcs import one_hot_enc, convert_to_numeric, split_torque_colum
 
 from settings import MODEL_NAME, SCALER_NAME, OHE_NAME
 
-model = MODEL_NAME
-scaler = SCALER_NAME
-ohe = OHE_NAME
+model = joblib.load(MODEL_NAME)
+scaler = joblib.load(SCALER_NAME)
+ohe = joblib.load(OHE_NAME)
+
 
 from fastapi import APIRouter, UploadFile
 
