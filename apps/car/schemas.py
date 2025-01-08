dc9ad01da3a834e2707069f5b4ca248c4e264d01
Mel-Rocha <rochamel73@gmail.com>
2025-01-08 08:22:39 -0300

    validação no campo mileage, o cálculo não é feito caso o referido campo esteja em formato inválido

--- apps/car/schemas.py
+++ apps/car/schemas.py
@@ -1,5 +1,5 @@
 from typing import List
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from fastapi import  UploadFile
 
 
@@ -17,6 +17,16 @@ class Item(BaseModel):
     max_power: str
     torque: str
     seats: float
+
+    @validator("mileage")
+    def validate_mileage(cls, value):
+        try:
+            parts = value.split()
+            if len(parts) != 2 or not parts[0].replace(".", "", 1).isdigit() or not parts[1].isalpha():
+                raise ValueError("Mileage deve estar no formato 'valor unidade', como '23.4 kmpl'.")
+        except Exception as e:
+            raise ValueError(f"Mileage inválido: {value}. {str(e)}")
+        return value
 
 
 class Items(BaseModel):
